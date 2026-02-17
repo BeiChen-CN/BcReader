051d7d962c3069fdb66824132b3cb730340b400e
youshen2 <178326018+youshen2@users.noreply.github.com>
2026-02-17 20:02:06 +0800

    并不会

--- src/utils/handshake.js
+++ src/utils/handshake.js
@@ -2,8 +2,8 @@ import interconn from './interconn.js';
 import { versionCode } from "../manifest.json";
 import router from '@system.router';
 
-const MIN_PHONE_VERSION = 126104;
-const MIN_PHONE_VERSION_NAME = "V26.1.4";
+const MIN_PHONE_VERSION = 126105;
+const MIN_PHONE_VERSION_NAME = "V26.1.5";
 //握握手，握握双手
 const type = "__hs__"
 const TIMEOUT = 15000;
