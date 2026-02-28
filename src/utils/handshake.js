4c89319dbd5d7603a0a1253122b87b3b1bd8ceea
youshen2 <178326018+youshen2@users.noreply.github.com>
2026-02-28 18:16:06 +0800

    feat:优化稳定性

--- src/utils/handshake.js
+++ src/utils/handshake.js
@@ -2,19 +2,17 @@ import interconn from './interconn.js';
 import { versionCode } from "../manifest.json";
 import router from '@system.router';
 
-const MIN_PHONE_VERSION = 126105;
-const MIN_PHONE_VERSION_NAME = "V26.1.5";
-//握握手，握握双手
+const MIN_PHONE_VERSION = 126200;
+const MIN_PHONE_VERSION_NAME = "V26.2.0";
 const type = "__hs__"
 const TIMEOUT = 15000;
 
 export default class InterHandshake extends interconn {
-    /** @type {Promise<void>} */
     promise = null;
-    /** @type {(value: void | PromiseLike<void>) => void} */
     resolve = null;
     timeout = null;
     handshaked = false;
+
     constructor() {
         super();
         this.conn.onmessage = ({ data }) => {
@@ -24,66 +22,79 @@ export default class InterHandshake extends interconn {
                 this.handshaked = false;
             }, TIMEOUT);
             const { tag, ...payload } = JSON.parse(data);
-            this.callbacks[tag](payload);
+            if (this.callbacks[tag]) {
+                this.callbacks[tag](payload);
+            }
         }
+        
         this.addListener(type, ({ count, version }) => {
             if ((version && version < MIN_PHONE_VERSION) || !version) {
                 const currentVersion = version || '未知';
-                const requiredVersion = MIN_PHONE_VERSION_NAME;
                 return router.replace({
                     uri: 'pages/confirm',
                     params: {
                         action: 'versionError',
                         title: '版本不兼容',
                         confirmText: '手机端版本过低',
-                        subText: `当前版本：${currentVersion}\n所需版本：${requiredVersion}\n请升级手机端后再使用本小程序`,
+                        subText: `当前版本：${currentVersion}\n所需版本：${MIN_PHONE_VERSION_NAME}\n请升级手机端后再使用本小程序`,
                     }
                 });
             }
             if (count > 0) {
                 this.handshaked = true;
-                if (this.promise) this.resolve(this.resolve = null)
-                else {
-                    this.promise = Promise.resolve()
-                    this.callback()
+                if (this.promise) {
+                    this.resolve();
+                    this.resolve = null;
+                } else {
+                    this.promise = Promise.resolve();
+                    this.callback();
                 }
             }
             if (count++ < 2) super.send(type, { count, version: versionCode });
-        })
+        });
+
         this.addEventListener((e) => {
             if (e !== "open") {
                 this.resolve = null;
                 this.promise = null;
                 this.handshaked = false;
                 clearTimeout(this.timeout);
-                return
+                return;
             }
             this.handshaked = false;
-            this.promise = this._newPromise()
-        })
+            this.promise = this._newPromise();
+        });
     }
+
     async send(...args) {
-        if (this.promise) await this.promise;
-        else await (this.promise = this._newPromise())
-        return await super.send(...args)
+        if (!this.promise) {
+            this.promise = this._newPromise();
+        }
+        await this.promise;
+        return await super.send(...args);
     }
+
     setHandshakeListener(callback) {
-        this.callback= callback
+        this.callback = callback;
     }
+
     callback = () => { }
-    get connected() { return this.handshaked }
+
+    get connected() { return this.handshaked; }
+
     _newPromise() {
-        return new Promise(( resolve, reject ) => {
+        return new Promise((resolve, reject) => {
             const timeout = setTimeout(() => {
-                reject(new Error("timeout"));
                 this.promise = this.resolve = null;
                 this.handshaked = false;
-            }, TIMEOUT)
+                reject(new Error("timeout"));
+            }, TIMEOUT);
+            
             this.resolve = () => {
-                resolve()
-                clearTimeout(timeout)
-            }
-            super.send(type, { count: 0, version: versionCode })
-        })
+                clearTimeout(timeout);
+                resolve();
+            };
+            super.send(type, { count: 0, version: versionCode });
+        });
     }
 }
